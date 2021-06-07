ce19f16f422331992249580dd80c3dd5335db55f
Muskan Handa <500068628@stu.upes.ac.in>
2021-06-07 12:15:46 +0530

    Add files via upload

---
+++ prep11.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+int n;
+cin>>n;
+int arr[n];
+    for(int i=0;i<n;i++)
+    {
+    cin>>arr[i];
+    }
+int left[n], right[n], result=0;
+left[0]=arr[0];
+    for(int i=1; i<n;i++)
+    {
+    if(arr[i]>left[i-1])
+    left[i]=arr[i];
+    else
+    left[i]=left[i-1];
+    }
+    right[n-1]=arr[n-1];
+    for(int i=n-1;i>=0;i--)
+    {
+    if(arr[i]>right[i+1])
+    right[i]=arr[i];
+    else
+    right[i]=right[i+1];
+    }
+    for(int i=1;i<n-1;i++)
+    {
+    result+=min(left[i],right[i])-arr[i];
+    }
+    cout<<result;
+return 0;
+}
+
